47ec0985212be751f8347e6b938aeca7da3216d9
Keshav Shetty <keshavshetty@gmail.com>
2025-07-29 14:59:04 +0530

    First try to fetch from Snapshot table

--- AlgoClient/src/main/java/com/nexcorio/algo/core/BaseClass.java
+++ AlgoClient/src/main/java/com/nexcorio/algo/core/BaseClass.java
@@ -581,12 +581,12 @@ public class BaseClass {
 			conn = HDataSource.getReadOnlyConnection();
 			Statement stmt = conn.createStatement();
 			
-			String fetchSql = "select DISTINCT(trading_symbol) as trading_symbol from nexcorio_option_greeks"
+			// First try to fetch from Snapshot table
+			String fetchSql = "select DISTINCT(trading_symbol) as trading_symbol from nexcorio_option_snapshot"
 					+ " where trading_symbol like '" + mainInstrument.getShortName() + "%' "
-					+ " and quote_time > '" + postgresShortDateFormat.format(getCurrentTime()) + " 09:15:00'"
-					+ " and quote_time < '" + postgresShortDateFormat.format(getCurrentTime()) + " 15:15:00'";
+					+ " and record_date = '" + postgresShortDateFormat.format(getCurrentTime()) + "'";
 			
-			//fileLogTelegramWriter.write("1. fetchSql="+fetchSql);
+			fileLogTelegramWriter.write("1. fetchSql="+fetchSql);
 			
 			List<String> optionnames = new ArrayList<>();			
 			ResultSet rs = stmt.executeQuery(fetchSql);
@@ -594,7 +594,23 @@ public class BaseClass {
 				optionnames.add(rs.getString("trading_symbol"));
 			}
 			rs.close();
-			//fileLogTelegramWriter.write("optionnames.size="+optionnames.size());
+			
+			if (optionnames.size()==0) { // not found in snapshot
+						
+				fetchSql = "select DISTINCT(trading_symbol) as trading_symbol from nexcorio_option_greeks"
+						+ " where trading_symbol like '" + mainInstrument.getShortName() + "%' "
+						+ " and quote_time > '" + postgresShortDateFormat.format(getCurrentTime()) + " 09:15:00'"
+						+ " and quote_time < '" + postgresShortDateFormat.format(getCurrentTime()) + " 15:15:00'";
+				
+				//fileLogTelegramWriter.write("1. fetchSql="+fetchSql);
+							
+				rs = stmt.executeQuery(fetchSql);
+				while (rs.next()) {
+					optionnames.add(rs.getString("trading_symbol"));
+				}
+				rs.close();
+				//fileLogTelegramWriter.write("optionnames.size="+optionnames.size());
+			}
 			
 			List<OptionGreek> ceOptionGreeks = new ArrayList<>();
 			List<OptionGreek> peOptionGreeks = new ArrayList<>();
