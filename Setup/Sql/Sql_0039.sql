c1264c19620a0e4a1b8b10f63d5e782296fdf3da
Keshav Shetty <keshavshetty@gmail.com>
2025-09-26 23:04:26 +0530

    missed fields

--- Setup/Sql/Sql_0039.sql
+++ Setup/Sql/Sql_0039.sql
@@ -7,4 +7,8 @@ ADD COLUMN minGammaExposure REAl DEFAULT 0,
 ADD COLUMN maxGammaExposure REAl DEFAULT 0,
 ADD COLUMN netGammaExposure REAl DEFAULT 0;
 
+ALTER TABLE nexcorio_option_atm_movement_data
+ADD COLUMN mingammaexposurestrike REAl DEFAULT 0,
+ADD COLUMN maxgammaexposurestrike REAl DEFAULT 0;
+
 INSERT INTO db_versions VALUES ('0039', now(), 'Keshav', 'Additional column for ATM data', 'Schema');
